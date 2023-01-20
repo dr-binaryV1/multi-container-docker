5f1b4ebafed3b0044f1d272d9f495ba607fadeff
Damian Wynter <damian@cllctve.com>
2023-01-20 01:23:48 -0400

    terminal listen line

--- worker/index.js
+++ worker/index.js
@@ -17,10 +17,12 @@ function fib(index) {
   return fib(index - 1) + fib(index - 2)
 }
 
+sub.on("message", (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message)))
+});
+
 sub.subscribe('insert', (error, message) => {
   if (error) {
     console.log(error)
   }
-  console.log(message)
-  redisClient.hset('values', message, fib(parseInt(message)))
 })
